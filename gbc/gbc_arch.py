2dcd5797f4d1532eda404be2eb9fdd6c33128ffc
Jarosław Kulesza <jkulesza@grupazpr.pl>
2023-04-30 20:33:36 +0200

    new file variants and cleaning

--- gbc/gbc_arch.py
+++ gbc/gbc_arch.py
@@ -1,3 +1,4 @@
+import os
 import re
 from pathlib import Path
 from typing import List, Tuple
@@ -14,22 +15,20 @@ INFO_FILE_NAME = "VERSION.NFO"
 SOURCE_ENCONDING = "cp1250"
 FULL_YEAR_LENGTH = 4
 
-SOURCE = "/mnt/d/Games"
-DEST = "/mnt/c/opt/gbc/"
-
 NAME_PATTERN = r"Name:\s+(.*)\r"
 PUBLISHER_PATTERN = r"Published:\s+([\d?]+)\s(.*)"
 
-FAT_PATTERN = re.compile(r"[^A-Za-z0-9 \$\%\-\_\!\(\)\{\}\^\#\&]")
+FAT_PATTERN = re.compile(r"[^A-Za-z0-9 \$\%\-\_\!\(\)\{\}\^\#\&\$\+]")
 
 
 @click.command()
 @click.argument("source_path")
 @click.argument("dest_path")
-def move_games(source_path, dest_path):
+def move_games(source_path: str, dest_path: str):
     source = Path(source_path)
     if not source.exists():
         raise click.ClickException("SOURCE PATH does not exists")
+
     dest = Path(dest_path)
     if not dest.exists():
         raise click.ClickException("DESTINATION PATH does not exists")
@@ -46,31 +45,50 @@ def move_games(source_path, dest_path):
             )
             dest_with_pub = prepare_dest_dir(publisher, PUBLISHER_FOLDER_NAME, dest)
 
-            for zip_fle in ZipPath(z).iterdir():
-                if zip_fle.name == INFO_FILE_NAME:
-                    continue
-
-                extract_to(z, zip_fle, dest_path_abc, name_with_year)
-                extract_to(z, zip_fle, dest_with_pub, name_with_year)
+            files = get_program_files(fle, z)
+            for i, zip_fle in enumerate(files):
+                file_number = f" { i + 1 }" if len(files) > 1 else ""
+                extract_to(z, zip_fle, dest_path_abc, name_with_year, file_number)
+                extract_to(z, zip_fle, dest_with_pub, name_with_year, file_number)
 
                 if _is_full_year(year):
                     dest_with_year = prepare_dest_dir(year, YEAR_FOLDER_NAME, dest)
-                    extract_to(z, zip_fle, dest_with_year, game)
+                    extract_to(z, zip_fle, dest_with_year, game, file_number)
 
 
-def _is_full_year(year):
+def get_program_files(fle, z):
+    return [fle for fle in ZipPath(z).iterdir() if fle.name != INFO_FILE_NAME]
+
+
+def _is_full_year(year: str) -> bool:
     return year.isdigit() and len(year) == FULL_YEAR_LENGTH
 
 
-def extract_to(z, zip_fle, dest_path, name):
+def extract_to(
+    z: ZipFile, zip_fle: ZipPath, dest_path: Path, name: str, file_number: str
+):
     extracted = z.extract(zip_fle.name, dest_path)
     extracted_path = Path(extracted)
-    extracted_path.rename(
-        extracted_path.parent / (name + extracted_path.suffix).strip()
+    final_path = (
+        extracted_path.parent / (name + file_number + extracted_path.suffix).strip()
     )
+    if final_path.exists():
+        final_path = (
+            extracted_path.parent
+            / (
+                name
+                + file_number
+                + " ("
+                + zip_fle.name.split(".")[0]
+                + ")"
+                + extracted_path.suffix
+            ).strip()
+        )
+
+    extracted_path.rename(final_path)
 
 
-def prepare_dest_dir(folder_name, parent, root):
+def prepare_dest_dir(folder_name: str, parent: str, root: Path) -> Path:
     parent_dir = root / parent.strip()
     if not parent_dir.exists():
         parent_dir.mkdir()
@@ -81,13 +99,13 @@ def prepare_dest_dir(folder_name, parent, root):
     return dest_path
 
 
-def get_abc_name(name_prefix):
+def get_abc_name(name_prefix: str) -> str:
     if not name_prefix.isalpha():
         name_prefix = "0"
     return name_prefix.lower()
 
 
-def extract_prefix(game):
+def extract_prefix(game: str) -> str:
     name_prefix = game[:2].encode("ascii", "ignore").decode()
     if len(name_prefix) == 2 and not name_prefix[1].isalpha():
         name_prefix = name_prefix[0]
--- tests/test_gbc.py
+++ tests/test_gbc.py
@@ -11,22 +11,45 @@ def test_move_games(tmp_path: Path):
     runner = CliRunner()
     result = runner.invoke(move_games, [source_dir, str(tmp_path)])
 
+    print(result.exception)
     assert result.exit_code == 0
-    assert dir_content(tmp_path) == [
-        "ABC",
-        "Years",
-        "Publishers",
-        "ABC/ba",
-        "ABC/ea",
-        "ABC/ba/Battle The (1986).T64",
-        "ABC/ea/Eaglehunt.T64",
-        "Years/1986",
-        "Years/1986/Battle The.T64",
-        "Publishers/(Not Published)",
-        "Publishers/Argus Specialist Publications LtdComputer Gamer",
-        "Publishers/(Not Published)/Eaglehunt.T64",
-        "Publishers/Argus Specialist Publications LtdComputer Gamer/Battle The (1986).T64",
-    ]
+    assert sorted(dir_content(tmp_path)) == sorted(
+        [
+            "ABC",
+            "Years",
+            "Publishers",
+            "ABC/0",
+            "ABC/ba",
+            "ABC/ea",
+            "ABC/0/0deg Nord (1985) 1.D64",
+            "ABC/0/0deg Nord (1985) 2.D64",
+            "ABC/0/0deg Nord (1985) 3.D64",
+            "ABC/0/17+4 (1985).T64",
+            "ABC/0/17+4 (1985) (17UND4V3).T64",
+            "ABC/ba/Battle The (1986).T64",
+            "ABC/ea/Eaglehunt.T64",
+            "Years/1986",
+            "Years/1985",
+            "Years/1986/Battle The.T64",
+            "Years/1985/0deg Nord 1.D64",
+            "Years/1985/0deg Nord 2.D64",
+            "Years/1985/0deg Nord 3.D64",
+            "Years/1985/17+4 (17UND4V3).T64",
+            "Years/1985/17+4.T64",
+            "Publishers/(Not Published)",
+            "Publishers/Argus Specialist Publications LtdComputer Gamer",
+            "Publishers/Ariolasoft",
+            "Publishers/(Not Published)/Eaglehunt.T64",
+            "Publishers/Argus Specialist Publications LtdComputer Gamer/Battle The (1986).T64",
+            "Publishers/Ariolasoft/0deg Nord (1985) 1.D64",
+            "Publishers/Ariolasoft/0deg Nord (1985) 2.D64",
+            "Publishers/Ariolasoft/0deg Nord (1985) 3.D64",
+            "Publishers/CW-Publikationen Verlags GmbHRUN",
+            "Publishers/CW-Publikationen Verlags GmbHRUN/17+4 (1985).T64",
+            "Publishers/S+S Soft Vertriebs GmbH",
+            "Publishers/S+S Soft Vertriebs GmbH/17+4 (1985).T64",
+        ]
+    )
 
 
 def test_source_path_does_not_exists(tmp_path: Path):
